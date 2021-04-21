d8a552564af9f006df1bdc350c7c3af489ae4621
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-04-21 23:45:52 +0530

    Initial commit, Adding first section

---
+++ script.js
@@ -0,0 +1,81 @@
+// Cube
+let x = 0;
+let y = 20;
+let z = 0;
+let bool = true;
+let interval;
+const cube = document.querySelector('.cube');
+document.querySelector('.top-x-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+document.querySelector('.bottom-x-control').addEventListener('click', () => {
+    cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+document.querySelector('.left-y-control').addEventListener('click', () => {
+    cube.style.transform = `rotateY(${(y -= 20)}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+});
+document.querySelector('.right-y-control').addEventListener('click', () => {
+    cube.style.transform = `rotateY(${(y += 20)}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+});
+document.querySelector('.top-z-control').addEventListener('click', () => {
+    cube.style.transform = `rotateY(${y}deg) rotateX(${x}deg) rotateZ(${(z -= 20)}deg)`;
+});
+document.querySelector('.bottom-z-control').addEventListener('click', () => {
+    cube.style.transform = `rotateY(${y}deg) rotateX(${x}deg) rotateZ(${(z += 20)}deg)`;
+});
+
+//
+const platPause = function () {
+    if (bool) {
+        interval = setInterval(() => {
+            cube.style.transform = `rotateY(${y++}deg) rotateX(${x}deg) rotateZ(${z}deg) `;
+        }, 100);
+    } else {
+        clearInterval(interval);
+    }
+};
+
+platPause();
+document.querySelector('.controls').addEventListener('mouseover', () => {
+    bool = false;
+    platPause();
+});
+document.querySelector('.controls').addEventListener('mouseout', () => {
+    bool = true;
+    platPause();
+});
+// End of Cube
+
+// Slideshow
+const slideshowDivs = () => {
+    for (let i = 1; i <= 5; i++) {
+        const div = document.createElement('div');
+        div.style.backgroundImage = `url(images/slideshow/section-1-bg-${i}.jpg)`;
+
+        i === 1 && div.classList.add('change');
+
+        document.querySelector('.slideshow').appendChild(div);
+    }
+};
+
+slideshowDivs();
+
+const divs = document.querySelectorAll('.slideshow div');
+let a = 1;
+
+const slideshow = () => {
+    setInterval(() => {
+        a++;
+        const div = document.querySelector('.slideshow .change');
+        div.classList.remove('change');
+        if (a < divs.length) {
+            div.nextElementSibling.classList.add('change');
+        } else {
+            divs[0].classList.add('change');
+            a = 1;
+        }
+    }, 20000);
+};
+
+slideshow();
+// End of Slideshow
