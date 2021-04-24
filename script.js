84a1657873d2c867b71aec719864923231645aa5
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-04-24 15:03:21 +0530

    Adding Airpods, footer  section and making responsive

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Common JS
-document.querySelectorAll('.watch-control, .controls a').forEach(control =>
-    control.addEventListener('click', e => {
-        e.preventDefault();
-    })
-);
+document
+    .querySelectorAll('.watch-control, .controls a, .iphone-btn')
+    .forEach(control =>
+        control.addEventListener('click', e => {
+            e.preventDefault();
+        })
+    );
 // End of Common JS
 
 // Cube
