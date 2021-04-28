882a046fc0222fe60e7f2bf336d49981950ccfa8
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-04-28 18:50:01 +0530

    Fixing iphone box rotation

--- script.js
+++ script.js
@@ -22,23 +22,23 @@ document.querySelector('.bottom-x-control').addEventListener('click', () => {
     cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
 });
 document.querySelector('.left-y-control').addEventListener('click', () => {
-    cube.style.transform = `rotateY(${(y -= 20)}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+    cube.style.transform = ` rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg)`;
 });
 document.querySelector('.right-y-control').addEventListener('click', () => {
-    cube.style.transform = `rotateY(${(y += 20)}deg) rotateX(${x}deg) rotateZ(${z}deg)`;
+    cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg)`;
 });
 document.querySelector('.top-z-control').addEventListener('click', () => {
-    cube.style.transform = `rotateY(${y}deg) rotateX(${x}deg) rotateZ(${(z -= 20)}deg)`;
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg)`;
 });
 document.querySelector('.bottom-z-control').addEventListener('click', () => {
-    cube.style.transform = `rotateY(${y}deg) rotateX(${x}deg) rotateZ(${(z += 20)}deg)`;
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg)`;
 });
 
 //
 const platPause = function () {
     if (bool) {
         interval = setInterval(() => {
-            cube.style.transform = `rotateY(${y++}deg) rotateX(${x}deg) rotateZ(${z}deg) `;
+            cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg) `;
         }, 100);
     } else {
         clearInterval(interval);
